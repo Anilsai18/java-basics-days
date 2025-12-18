d5b1edd0a3ff6571db7e6b524cb05e4785290a64
Anilsai18 <chary123.sai@gmail.com>
2025-12-18 10:45:41 +0530

    Day2 elseif statement

---
+++ ElseIf.java
@@ -0,0 +1,14 @@
+public class ElseIf{
+public static void main(String [] args){
+int marks=92;
+if(marks>=90){
+System.out.println("Grade A");
+}else if(marks>=80){
+System.out.println("Grade B ");
+}else if(marks>=65){
+System.out.println("Grade C");
+}else{
+System.out.println("Fail");
+}
+}
+}
